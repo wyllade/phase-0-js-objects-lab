cd6116466e78507061fd90801c79408d5371989b
Emman Oduor <wyllademanu003@gmail.com>
2025-11-21 09:06:46 +0300

    Your commit message

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+const logAttendeeName = (attendee) => {
+  console.log(attendee.name);
+}
 
+const logTicketPrice = (attendee) => {
+  console.log(attendee.ticketPrice);
+}
+
+const updateTicketType = (attendee, newType) => {
+  attendee.ticketType = newType;
+}
+const updateTicketPrice = (attendee, newPrice) => {
+  attendee.ticketPrice = newPrice;
+}
+const removeEventProperty = (attendee) => {
+  delete attendee.event;
+}
+
+const addCheckedInProperty = (attendee) => {
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
